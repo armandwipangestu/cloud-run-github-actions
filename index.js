96e109e21721f679c1d0650d3a945e124b4dd794
Arman <armandwi.pangestu7@gmail.com>
2024-05-06 14:41:47 +0700

    test(message): change 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.status(200).send({
         status: "success",
-        message: "Hello World from Cloud Run",
+        message: "Hello World from Cloud Run Edited 1",
     });
 });
 
